2fa1f50ddb90fc0629556d2342cdc62121eb7d6e
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2023-12-11 14:22:10 +0700

    update

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chủ</title>
+</head>
+<body>
+    <?php include_once('components/header.php') ?>
+    <div class="bannerHome">
+        <img src="../public/images/bannerHome.jpg" alt="">
+    </div> 
+</body>
+</html>
