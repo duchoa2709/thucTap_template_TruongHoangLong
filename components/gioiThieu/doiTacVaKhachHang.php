835729f333be53ad92d562fa431e3deb655e1a3d
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2024-01-08 11:30:00 +0700

    end giới thiệu

--- components/gioiThieu/doiTacVaKhachHang.php
+++ components/gioiThieu/doiTacVaKhachHang.php
@@ -1,7 +1,87 @@
 <style>
+.bg-doiTacVaKhachHang {
+    background-image: url('./public/images/history/map.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    height: 650px;
+}
 
+.doitac .img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    /* center */
+    display: block;
+    margin: auto;
+}
 </style>
 
-<div class="container">
-    
+<div class="bg-doiTacVaKhachHang mb-40">
+    <div class="container px-40 mx-auto my-40 congTyThanhVien">
+        <div class="text-center bg-partners">
+            <div class="text-stone-500 font-thin">
+                HỢP TÁC & PHÁT TRIỂN
+            </div>
+            <p class="text-[#1b2c65] text-5xl font-bold">
+                ĐỐI TÁC
+                <span class=" font-thin">
+                    &
+                </span>
+                KHÁCH HÀNG
+            </p>
+        </div>
+        <!-- đối tác -->
+        <div class="doitac mt-16">
+            <div class="flex">
+                <div class="w-1/5 p-3 border-r flex items-center justify-center">
+                </div>
+                <!-- Lặp lại cho các phần tử khác, nhưng không thêm border cho phần tử cuối cùng -->
+                <div class="w-1/5 p-3 border-r flex items-center justify-center">
+                    <img src="./public/images/history/1.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-r flex items-center justify-center">
+                    <img src="./public/images/history/2.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-r flex items-center justify-center">
+                    <img src="./public/images/history/3.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 flex items-center justify-center">
+                </div>
+            </div>
+            <div class="flex">
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                    <img src="./public/images/history/4.png" alt="">
+                </div>
+                <!-- Lặp lại cho các phần tử khác, nhưng không thêm border cho phần tử cuối cùng -->
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                    <img src="./public/images/history/12.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                    <img src="./public/images/history/6.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                    <img src="./public/images/history/7.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-t flex items-center justify-center">
+                    <img src="./public/images/history/8.png" alt="">
+                </div>
+            </div>
+            <div class="flex">
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                </div>
+                <!-- Lặp lại cho các phần tử khác, nhưng không thêm border cho phần tử cuối cùng -->
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                    <img src="./public/images/history/9.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                    <img src="./public/images/history/10.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-r border-t flex items-center justify-center">
+                    <img src="./public/images/history/11.png" alt="">
+                </div>
+                <div class="w-1/5 p-3 border-t flex items-center justify-center">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
