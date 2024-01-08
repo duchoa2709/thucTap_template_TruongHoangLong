35303a87533c7fde4f93d4f7fcea9fe9d6cf13aa
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2024-01-08 14:39:09 +0700

    update

---
+++ linhVucKinhDoanh.php
@@ -0,0 +1,87 @@
+<!-- =============================================================== -->
+<!-- Header navbar -->
+<?php include_once('components/header.php') ?>
+<!-- =============================================================== -->
+
+<!-- =============================================================== -->
+<!-- anh1 -->
+<div class="flex bg-[url('./public/images/linhVucKinhDoanh/anh1.png')] bg-no-repeat bg-cover bg-center h-[650px]">
+    <div class="w-1/5"></div>
+    <div class="w-1/5 bg-[#1B2C65] relative">
+        <img src="./public/images/linhVucKinhDoanh/SERVICE.png" alt="" class="h-[456px] float-right">
+        <div class="px-16 py-24 flex flex-col absolute bottom-0 left-0">
+            <img src="./public/images/linhVucKinhDoanh/icon1.png" alt="" class="h-[90px] w-[90px]">
+            <p class="text-[#FFFFFF] text-[30px] font-extralight">
+                LĨNH VỰC
+            </p>
+            <p class="text-[#FFFFFF] text-[30px] font-bold">
+                BẤT ĐỘNG SẢN
+            </p>
+        </div>
+        <button class="absolute bottom-0 right-0 bg-[#CF4735] text-[#fff] text-5xl font-thin px-4 py-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+        </button>
+    </div>
+    <div class="w-3/5"></div>
+</div>
+<!-- =============================================================== -->
+
+<!-- =============================================================== -->
+<!-- anh2 -->
+<div class="flex bg-[url('./public/images/linhVucKinhDoanh/anh2.png')] bg-no-repeat bg-cover bg-center h-[650px]">
+    <div class="w-1/5 bg-[#1B2C65] relative">
+        <img src="./public/images/linhVucKinhDoanh/SERVICE.png" alt="" class="h-[456px] float-right">
+        <div class="px-16 py-24 flex flex-col absolute bottom-0 left-0">
+            <img src="./public/images/linhVucKinhDoanh/icon2.png" alt="" class="h-[90px] w-[90px]">
+            <p class="text-[#FFFFFF] text-[30px] font-extralight">
+                LĨNH VỰC
+            </p>
+            <p class="text-[#FFFFFF] text-[30px] font-bold">
+                BẤT ĐỘNG SẢN
+            </p>
+        </div>
+        <button class="absolute bottom-0 right-0 bg-[#CF4735] text-[#fff] text-5xl font-thin px-4 py-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+        </button>
+    </div>
+    <div class="w-1/5"></div>
+    <div class="w-3/5"></div>
+</div>
+<!-- =============================================================== -->
+
+<!-- =============================================================== -->
+<!-- anh3 -->
+<div class="flex bg-[url('./public/images/linhVucKinhDoanh/anh3.png')] bg-no-repeat bg-cover bg-center h-[650px]">
+    <div class="w-1/5"></div>
+    <div class="w-1/5 bg-[#1B2C65] relative">
+        <img src="./public/images/linhVucKinhDoanh/SERVICE.png" alt="" class="h-[456px] float-right">
+        <div class="px-16 py-24 flex flex-col absolute bottom-0 left-0">
+            <img src="./public/images/linhVucKinhDoanh/icon3.png" alt="" class="h-[90px] w-[90px]">
+            <p class="text-[#FFFFFF] text-[30px] font-extralight">
+                LĨNH VỰC
+            </p>
+            <p class="text-[#FFFFFF] text-[30px] font-bold">
+                BẤT ĐỘNG SẢN
+            </p>
+        </div>
+        <button class="absolute bottom-0 right-0 bg-[#CF4735] text-[#fff] text-5xl font-thin px-4 py-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+        </button>
+    </div>
+    <div class="w-3/5"></div>
+</div>
+<!-- =============================================================== -->
+
+<!-- =============================================================== -->
+<!-- footer -->
+<?php include_once('components/footer.php') ?>
+<!-- =============================================================== -->
