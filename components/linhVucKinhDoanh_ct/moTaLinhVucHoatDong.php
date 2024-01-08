b32509f96c55b1a9709ce1f9d980dba7814aa4f7
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2024-01-08 16:11:41 +0700

    update

---
+++ components/linhVucKinhDoanh_ct/moTaLinhVucHoatDong.php
@@ -0,0 +1,33 @@
+<div class="container mx-auto px-40 mt-40">
+    <div class="flex gap-56 pt-10 py-32 relative">
+        <img src="./public/images/linhVucKinhDoanh_ct/polygon-right.png" alt="" class="absolute top-[15rem] right-[-17rem]">
+        <div class="w-1/2">
+            <div class="">
+                <p class="text-[#666666] text-xl relative">
+                    <img src="./public/images/linhVucKinhDoanh_ct/SERVICE.png" alt=""
+                        class="absolute top-[-24px] left-0">
+                    VỀ CHÚNG TÔI
+                </p>
+                <div class="pt-5">
+                    <p class="text-[#1B2C65] leading-8 text-4xl">LĨNH VỰC HOẠT ĐỘNG</p>
+                    <p class="text-[#1B2C65] leading-8 text-4xl font-bold pt-3">XÂY DỰNG</p>
+                </div>
+                <div class="text-stone-500 text-[15px] leading-relaxed pt-20 space-y-4">
+                    <p>Hoạt động Đầu tư tài chính là bước phát triển chiến lược, phù hợp với xu hướng của thời đại, là
+                        bước
+                        tạo đà quan trọng để HIQ Land đạt được các mục tiêu phát triển trong tương lai.</p>
+                    <p>Bước đi đầu tiên sẽ là thiết lập mối quan hệ, hợp tác với các công ty/quỹ đầu tư tài chính uy
+                        tín (như Vietinbank…) để có nguồn vốn đầu tư vào các dự án.</p>
+                    <p>Qua việc đầu tư vốn vào công ty thành viên đã có mối liên kết chặt chẽ, có chung lợi ích kinh
+                        tế sẽ tạo điều kiện tối đa hóa lợi nhuận, đồng thời phân tán rủi ro.</p>
+                </div>
+            </div>
+        </div>
+        <div class="w-1/2">
+            <div class="relative">
+                <img class="w-[469px] h-[520px]" src="./public/images/linhVucKinhDoanh_ct/anh1.png" />
+                <img class="w-[282px] h-[255px] absolute bottom-[-8rem] left-[-8rem]" src="./public/images/linhVucKinhDoanh_ct/anh2.png" />
+            </div>
+        </div>
+    </div>
+</div>
