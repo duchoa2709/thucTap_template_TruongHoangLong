6cf70d01362e8e5528d383021b91b07818865dc0
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2024-01-09 14:46:02 +0700

    update

---
+++ components/linhVucKinhDoanh_ct/viSaoLaiChon.php
@@ -0,0 +1,109 @@
+<style>
+.polygon-left {
+    position: relative;
+}
+
+.polygon-left::before {
+    content: "";
+    top: -15rem;
+    left: -21.3rem;
+    position: absolute;
+    width: 22rem;
+    height: 30rem;
+    background: url("./public/images/polygonTrai.png");
+    z-index: 1;
+    background-repeat: no-repeat;
+}
+
+.viSaoChon {
+    position: relative;
+}
+
+.viSaoChon::before {
+    content: "";
+    width: 90%;
+    height: 3px;
+    background: #1B2C65;
+    position: absolute;
+    top: 17rem;
+    left: 0;
+}
+</style>
+
+<div class="container mx-auto px-40 mt-40 mb-20 polygon-left">
+    <div class="text-center">
+        <div class="text-center bg-left-polygon">
+            <div class="relative">
+                <img src="./public/images/linhVucKinhDoanh_ct/REPUTATION.png" alt=""
+                    class="absolute top-[7px] left-[17rem]">
+                <p class="text-[#666666] text-xl">
+                    TRƯƠNG HOÀNG LONG
+                </p>
+                <p class="text-[#1B2C65] leading-8 text-5xl mt-4 mb-8 font-light">
+                    VÌ SAO CHỌN
+                    <span class="font-bold">
+                        CHÚNG TÔI
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="flex gap-10">
+        <div class="w-1/4 border-r pr-2 viSaoChon ">
+            <svg class="rounded-full border w-[100px] h-[100px] p-[20px] hover:bg-[#CF4735] hover:text-[#fff] transition-all duration-300 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+            </svg>
+            <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
+                An toàn - uy tín
+            </p>
+            <p class="text-stone-500 text-[15px] leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </p>
+        </div>
+        <div class="w-1/4 border-r pr-2 viSaoChon ">
+            <svg class="rounded-full border w-[100px] h-[100px] p-[20px] hover:bg-[#CF4735] hover:text-[#fff] transition-all duration-300 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+            </svg>
+            <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
+                An toàn - uy tín
+            </p>
+            <p class="text-stone-500 text-[15px] leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </p>
+        </div>
+        <div class="w-1/4 border-r pr-2 viSaoChon ">
+            <svg class="rounded-full border w-[100px] h-[100px] p-[20px] hover:bg-[#CF4735] hover:text-[#fff] transition-all duration-300 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+            </svg>
+            <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
+                An toàn - uy tín
+            </p>
+            <p class="text-stone-500 text-[15px] leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </p>
+        </div>
+        <div class="w-1/4 border-r pr-2 viSaoChon ">
+            <svg class="rounded-full border w-[100px] h-[100px] p-[20px] hover:bg-[#CF4735] hover:text-[#fff] transition-all duration-300 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+            </svg>
+            <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
+                An toàn - uy tín
+            </p>
+            <p class="text-stone-500 text-[15px] leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </p>
+        </div>
+    </div>
+</div>
