7d95281c9dc028d2e29b41e7acd780367407f2af
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2024-01-15 14:01:19 +0700

    update hoat dong

---
+++ components/linhVucKinhDoanh_ct/hoatDong.php
@@ -0,0 +1,123 @@
+<div class="container mx-auto px-40 mt-40 mb-20">
+    <div class="text-center">
+        <div class="text-center bg-left-polygon">
+            <div class="relative">
+                <img src="./public/images/linhVucKinhDoanh_ct/album.png" alt="" class="absolute top-[7px] left-[26rem]">
+                <p class="text-[#666666] text-xl">
+                    TRƯƠNG HOÀNG LONG
+                </p>
+                <p class="text-[#1B2C65] leading-8 text-5xl mt-4 mb-8 font-light">
+                    THƯ VIỆN
+                    <span class="font-bold">
+                        HÌNH ẢNH
+                    </span>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+.test .swiper {
+    width: 100%;
+    height: 677px;
+}
+
+.test .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1200px;
+    /* Set the width */
+    height: 677px;
+    /* Set the height */
+}
+
+.test .swiper-slide:after{
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 677px;
+    background: rgba(0, 0, 0, 0.5);
+}
+
+.test .swiper-slide-active:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 677px;
+    background: rgba(0, 0, 0, 0);
+
+}
+
+.test .swiper-wrapper img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+}
+
+.swiper-button-prev:after,
+.swiper-rtl .swiper-button-next:after {
+    content: 'prev';
+    color: white;
+    background-color: rgba(0, 0, 0, 0.2);
+    padding: 19px 20px;
+    font-size: 18px;
+    font-weight: 700;
+    margin-left: 25rem;
+}
+
+.swiper-button-next:after, .swiper-rtl .swiper-button-prev:after {
+    content: 'next';
+    color: white;
+    background-color: rgba(0, 0, 0, 0.2);
+    padding: 19px 20px;
+    font-size: 18px;
+    font-weight: 700;
+    margin-right: 25rem;
+}
+</style>
+</head>
+
+<!-- Swiper -->
+<div class="test">
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="./public/images/linhVucKinhDoanh_ct/Rectangle 131.png" alt=""></div>
+            <div class="swiper-slide"><img src="./public/images/linhVucKinhDoanh_ct/Rectangle 134.png" alt=""></div>
+            <div class="swiper-slide"><img src="./public/images/linhVucKinhDoanh_ct/Rectangle 133.png" alt=""></div>
+            <div class="swiper-slide"><img src="./public/images/linhVucKinhDoanh_ct/Rectangle 133.png" alt=""></div>
+            <div class="swiper-slide"><img src="./public/images/linhVucKinhDoanh_ct/Rectangle 133.png" alt=""></div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+</div>
+<!-- Swiper JS -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- Initialize Swiper -->
+<script>
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+</script>
--- components/linhVucKinhDoanh_ct/viSaoLaiChon.php
+++ components/linhVucKinhDoanh_ct/viSaoLaiChon.php
@@ -68,10 +68,25 @@
                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+                    d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
             </svg>
             <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
-                An toàn - uy tín
+                Hợp tác phát triển
+            </p>
+            <p class="text-stone-500 text-[15px] leading-relaxed">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            </p>
+        </div>
+        <div class="w-1/4 border-r pr-2 viSaoChon ">
+            <svg class="rounded-full border w-[100px] h-[100px] p-[20px] hover:bg-[#CF4735] hover:text-[#fff] transition-all duration-300 ease-in-out"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+            </svg>
+            <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
+                Chất lượng
             </p>
             <p class="text-stone-500 text-[15px] leading-relaxed">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
@@ -82,24 +97,11 @@
                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
+                    d="M15.75 3.75 18 6m0 0 2.25 2.25M18 6l2.25-2.25M18 6l-2.25 2.25m1.5 13.5c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
             </svg>
+
             <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
-                An toàn - uy tín
-            </p>
-            <p class="text-stone-500 text-[15px] leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            </p>
-        </div>
-        <div class="w-1/4 border-r pr-2 viSaoChon ">
-            <svg class="rounded-full border w-[100px] h-[100px] p-[20px] hover:bg-[#CF4735] hover:text-[#fff] transition-all duration-300 ease-in-out"
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z" />
-            </svg>
-            <p class="text-[#1B2C65] leading-8 text-2xl mt-4 mb-8 font-bold">
-                An toàn - uy tín
+                Tiết kiệm chi phí
             </p>
             <p class="text-stone-500 text-[15px] leading-relaxed">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
