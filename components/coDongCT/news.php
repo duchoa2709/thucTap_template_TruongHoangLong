dab2a5e283c876869367eb081788bb75c24bd0b5
duchoa2709 <21211tt4819@mail.tdc.edu.vn>
2024-01-22 14:03:49 +0700

    update

---
+++ components/coDongCT/news.php
@@ -0,0 +1,122 @@
+<div class="container mx-auto px-40 z-50 mt-40 mb-52">
+    <div class="flex gap-36">
+        <div class="w-4/5">
+            <div class="text-blue-950 text-[45px] font-extrabold uppercase leading-[60px] border-b-2 pb-14">
+                THÔNG BÁO VỀ NGÀY ĐĂNG KÝ CUỐI CÙNG CHỐT QUYỀN NHẬN CỔ TỨC
+            </div>
+            <div class="flex pt-3 justify-between">
+                <div class="left flex gap-8">
+                    <div class="date flex justify-center items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                        </svg>
+                        <div class="ml-2">12/12/2021</div>
+                    </div>
+                    <div class="view flex justify-center items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                        <div class="ml-2">123</div>
+                    </div>
+                </div>
+                <div class="right flex gap-8">
+                    <a href="#">
+                        Chia sẻ
+                    </a>
+
+                    <!-- icon fb -->
+                    <a href="">
+                        <img src="./public/images/coDongCT/fb.svg" alt="">
+                    </a>
+
+                    <!-- icon twitter -->
+                    <a href="">
+                        <img src="./public/images/coDongCT/TwitterLogo.svg" alt="">
+                    </a>
+
+                    <!-- icon linkedin -->
+                    <a href="">
+                        <img src="./public/images/coDongCT/LinkedinLogo.svg" alt="">
+                    </a>
+                </div>
+            </div>
+            <!-- shot descripition -->
+            <div class="mt-10 text-xl leading-7">
+                <div class="text-zinc-800 font-bold">
+                    Thông báo về ngày đăng ký cuối cùng chốt quyền nhận cổ tức năm 2021 bằng cổ phiếu và cổ phiếu phát
+                    hành thêm.
+                </div>
+                <div class="mt-5">
+                    1. Trả cổ tức năm 2021 bằng cổ phiếu <br>
+                    - Tỷ lệ thực hiện: 100:17 (người sở hữu 100 cổ phiếu nhận được 17 cổ phiếu mới). <br>
+                    - Phương án xử lý cổ phiếu lẻ: Cổ phiếu phát hành thêm sẽ được làm tròn xuống đến hàng đơn vị, phần
+                    cổ phiếu lẻ (phần thập phân phát sinh nếu có) sẽ được hủy bỏ.<br>
+                    Ví dụ: Cổ đông Nguyễn Văn A sở hữu 789 cổ phiếu DIG sẽ được quyền nhận thêm: 789 x 17/100 = 134,13,
+                    làm tròn xuống thành 134 cổ phiếu DIG, phần cổ phiếu lẻ sẽ bị hủy.<br>
+                    - Địa điểm thực hiện:<br>
+                    <br>
+                    Đối với chứng khoán lưu ký: Người sở hữu sẽ làm thủ tục nhận cổ tức bằng cổ phiếu tại các Thành viên
+                    lưu ký nơi mở tài khoản lưu ký.<br>
+                    Đối với chứng khoán chưa lưu ký: Người sở hữu chứng khoán làm thủ tục nhận cổ tức bằng cổ phiếu tại
+                    Tổng Công ty Cổ phần Đầu tư Phát triển Xây dựng (DIC Group) – Số 15 Thi Sách, Phường Thắng Tam, TP.
+                    Vũng Tàu và xuất trình CMND/CCCD.<br>
+                    <br>
+                    2. Phát hành cổ phiếu để tăng vốn cổ phần từ nguồn vốn chủ sở hữu<br>
+                    <br>
+                    - Tỷ lệ thực hiện: 100:5 (người sở hữu 100 cổ phiếu nhận được 5 cổ phiếu mới).<br>
+                    - Phương án xử lý cổ phiếu lẻ: Cổ phiếu phát hành thêm sẽ được làm tròn xuống đến hàng đơn vị, phần
+                    cổ phiếu lẻ (phần thập phân phát sinh nếu có) sẽ được hủy bỏ.<br>
+                    Ví dụ: Cổ đông Nguyễn Văn A sở hữu 789 cổ phiếu DIG sẽ được quyền nhận thêm: 789 x 5/100 = 39,45,
+                    làm tròn xuống thành 39 cổ phiếu DIG, phần cổ phiếu lẻ sẽ bị hủy.<br>
+                    - Địa điểm thực hiện:<br>
+                    Đối với chứng khoán lưu ký: Người sở hữu sẽ làm thủ tục nhận cổ phiếu phát hành để tăng vốn cổ phần
+                    từ nguồn vốn chủ sở hữu tại các Thành viên lưu ký nơi mở tài khoản lưu ký.<br>
+                    Đối với chứng khoán chưa lưu ký: Người sở hữu chứng khoán làm thủ tục nhận cổ phiếu phát hành để
+                    tăng vốn cổ phần từ nguồn vốn chủ sở hữu tại Tổng Công ty Cổ phần Đầu tư Phát triển Xây dựng (DIC
+                    Group) – Số 15 Thi Sách, Phường Thắng Tam, TP. Vũng Tàu và xuất trình CMND/CCCD.<br>
+                    <br>
+                    <br>
+                    (Nguồn: VSD)
+                </div>
+                <!-- tag -->
+                <div class="mt-10 flex">
+                    <div class="text-zinc-800 mr-10">
+                        Thẻ:
+                    </div>
+                    <div class="flex gap-3 text-stone-500">
+                        <a href="#">
+                            Trương Hoàng Long
+                        </a>
+                        /
+                        <a href="#">
+                            BĐS
+                        </a>
+                        /
+                        <a href="#">
+                            Bất Động Sản
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-1/5">
+            <div class="aside-right">
+                <div class="text-blue-950 text-2xl font-bold uppercase leading-[60px] border-b-2 pb-14">
+                    Tin tức
+                </div>
+                <div class="text-blue-950 text-2xl font-bold uppercase leading-[60px] border-b-2 pb-14">
+                    Sự kiện
+                </div>
+                <div class="text-blue-950 text-2xl font-bold uppercase leading-[60px] border-b-2 pb-14">
+                    Tài liệu
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
